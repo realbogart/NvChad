1d1c3ed0a4c7a6e9f599679c807173cbd10ac26d
Johan Yngman <johan.yngman@paradoxinteractive.com>
2023-07-14 15:56:04 +0200

    Added more telescope bindings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,10 +8,15 @@ M.disabled = {
 
 M.abc = {
   n = {
-     ["<leader>gj"] = {"<cmd> Telescope <CR>", "Telescope"},
+     ["<leader>tt"] = {"<cmd> Telescope <CR>", "Telescope"},
+    -- Git bindings
      ["<leader>gf"] = {"<cmd> Telescope git_files <CR>", "Git files"},
      ["<leader>gs"] = {"<cmd> Telescope git_status <CR>", "Git status"},
      ["<leader>gc"] = {"<cmd> Telescope git_commits <CR>", "Git commits"},
+    -- LSP bindings
+     ["<leader>lr"] = {"<cmd> Telescope lsp_references<CR>", "LSP references"},
+     ["<leader>ld"] = {"<cmd> Telescope lsp_definitions<CR>", "LSP definitions"},
+     ["<leader>li"] = {"<cmd> Telescope lsp_implementations<CR>", "LSP implementations"},
   },
 }
 
