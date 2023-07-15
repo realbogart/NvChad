970e7f6f5ff73a393ce73c3ce651006a7b3d9502
Johan Yngman <johan.yngman@gmail.com>
2023-07-15 11:46:21 +0200

    Mapping for lsp rename

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,15 +8,21 @@ M.disabled = {
 
 M.abc = {
   n = {
-     ["<leader>tt"] = {"<cmd> Telescope <CR>", "Telescope"},
+    ["<leader>tt"] = {"<cmd> Telescope <CR>", "Telescope"},
     -- Git bindings
-     ["<leader>gf"] = {"<cmd> Telescope git_files <CR>", "Git files"},
-     ["<leader>gs"] = {"<cmd> Telescope git_status <CR>", "Git status"},
-     ["<leader>gc"] = {"<cmd> Telescope git_commits <CR>", "Git commits"},
+    ["<leader>gf"] = {"<cmd> Telescope git_files <CR>", "Git files"},
+    ["<leader>gs"] = {"<cmd> Telescope git_status <CR>", "Git status"},
+    ["<leader>gc"] = {"<cmd> Telescope git_commits <CR>", "Git commits"},
     -- LSP bindings
-     ["<leader>lr"] = {"<cmd> Telescope lsp_references<CR>", "LSP references"},
-     ["<leader>ld"] = {"<cmd> Telescope lsp_definitions<CR>", "LSP definitions"},
-     ["<leader>li"] = {"<cmd> Telescope lsp_implementations<CR>", "LSP implementations"},
+    ["<leader>lr"] = {"<cmd> Telescope lsp_references<CR>", "LSP references"},
+    ["<leader>ld"] = {"<cmd> Telescope lsp_definitions<CR>", "LSP definitions"},
+    ["<leader>li"] = {"<cmd> Telescope lsp_implementations<CR>", "LSP implementations"},
+    ["<leader>lc"] = {
+      function()
+        require("nvchad_ui.renamer").open()
+      end,
+      "LSP rename",
+    },
   },
 }
 
