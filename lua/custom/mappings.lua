2797bb33f655d963cdfa911fd0dbf28c4650486e
Johan Yngman <johan.yngman@gmail.com>
2023-07-15 22:45:34 +0200

    Configured lsp_references to include current line

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -14,7 +14,7 @@ M.abc = {
     ["<leader>gs"] = {"<cmd> Telescope git_status <CR>", "Git status"},
     ["<leader>gc"] = {"<cmd> Telescope git_commits <CR>", "Git commits"},
     -- LSP bindings
-    ["<leader>lr"] = {"<cmd> Telescope lsp_references<CR>", "LSP references"},
+    ["<leader>lr"] = {"<cmd> Telescope lsp_references include_current_line=true<CR>", "LSP references"},
     ["<leader>ld"] = {"<cmd> Telescope lsp_definitions<CR>", "LSP definitions"},
     ["<leader>li"] = {"<cmd> Telescope lsp_implementations<CR>", "LSP implementations"},
     ["<leader>lc"] = {
